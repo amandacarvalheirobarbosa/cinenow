77de71d77029e5b6498b733c4a02e6d60f8ce494
Henrique Barucco <henrique_barucco@hotmail.com>
2023-08-08 13:38:05 -0300

    feat(route.js): adiciona importação do módulo prisma fix(route.js): corrige indentação e formatação do código fix(page.js): corrige importação do componente NavBar fix(pageFav/page.js): corrige importação do componente NavBar fix(formLogin.js): corrige formatação e indentação do código fix(nabBar.js): corrige formatação e indentação do código fix(prisma.js): corrige importação do módulo prisma
    As alterações foram feitas para corrigir erros de formatação, indentação e importação de módulos. Essas alterações visam melhorar a legibilidade e manutenibilidade do código.

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,54 +1,52 @@
 import NextAuth from "next-auth/next";
 import { compare } from "bcryptjs";
 import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "src/lib/prisma";
 
 export const OPTIONS = {
-    session: {
-      strategy: "jwt",
-    },
-    providers: [
-      CredentialsProvider({
-        name: "Sign in",
-        credentials: {
-          email: {
-            label: "Email",
-            type: "email",
-            placeholder: "example@example.com",
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Sign in",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@example.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials.password) {
+          return null;
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
           },
-          password: { label: "Password", type: "password" },
-        },
-        async authorize(credentials) {
-          console.log(credentials);
+        });
 
-          if (!credentials?.email || !credentials.password) {
-            return null;
-          }
-  
-          const user = await prisma.user.findUnique({
-            where: {
-              email: credentials.email,
-            },
-          });
-          
-          console.log(user);
-          if (!user || !(await compare(credentials.password, user.password))) {
-            return null;
-          }
-  
-          return {
-            id: user.id,
-            email: user.email,
-            name: user.name
-          };
-        },
-      }),
-    ],
-    pages: {
-        signIn: '/login',
-        signOut: '/login',
-        error: '/login',
-    }
-  };
+        if (!user || !(await compare(credentials.password, user.password))) {
+          return null;
+        }
 
-const handler=NextAuth(OPTIONS);
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+        };
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+    signOut: "/login",
+    error: "/login",
+  },
+};
+
+const handler = NextAuth(OPTIONS);
 export { handler as GET, handler as POST };
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import NavBar from '@/components/nabBar'
+import NavBar from "../components/nabBar";
 
 export default function Home() {
   return (
     <>
-      <NavBar/>
+      <NavBar />
     </>
-  )
+  );
 }

e33132541eacecda7b055051e3ca07a318bf1195
Amanda Carvalheiro Barbosa <acarvalheirobarbosa@gmail.com>
2023-08-15 13:10:00 -0300

    change layout and language

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import Footer from "../components/footer";
 import NavBar from "../components/nabBar";
 
 async function getMovies() {
-  const movies = await fetch('https://api.themoviedb.org/3/movie/upcoming?language=pt-BR', {
+  const date = new Date().toISOString().split('T')[0];
+  const URL_BR: string = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=pt-BR&primary_release_date.gte=${date}&sort_by=popularity.desc`;
+  const URL_US: string = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&primary_release_date.gte=${date}&sort_by=popularity.desc`;
+  
+  const movies = await fetch(URL_BR, {
     headers: {
       'Authorization': `Bearer ${process.env.TOKEN}`,
       'accept': 'application/json'
@@ -14,7 +18,6 @@ async function getMovies() {
     throw new Error('Failed to fetch data');
   }
  
-  /* return JSON.parse(movies.json(), any[]); */
   return movies.json();
 }
 
@@ -25,14 +28,13 @@ export default async function Home() {
     <>
       <NavBar />
       <div className="flex flex-wrap">
-        {/* {movieList.map((movie) => <CardFilm key={movie.id} id={movie.id} title={movie.title} genres={movie.genres} overview={movie.overview} voteAverage={movie.vote_average} posterPath={movie.poster_path} releaseDate={movie.release_date}/>)} */}
         {movieList.results.map((movie) => (
           <CardFilm 
             key={movie.id} 
             id={movie.id} 
             title={movie.title} 
             genres={movie.genres} 
-            overview={movie.overview} 
+            overview={movie.overview || '⚠️ Ainda não há descrição em português desse filme.'} 
             voteAverage={movie.vote_average} 
             posterPath={movie.poster_path} 
             releaseDate={movie.release_date}
